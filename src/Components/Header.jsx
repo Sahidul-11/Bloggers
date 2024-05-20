254f241dddcac786b19ba98b5ee30acc76e4a48a
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-05-20 13:08:44 +0600

    framer motion done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,12 @@ import { DarkThemeToggle, Flowbite } from "flowbite-react";
 import { Link, NavLink } from 'react-router-dom';
 import UseContext from '../Hooks/UseContext';
 import Swal from 'sweetalert2';
+import "../Pages/style.css"
 
 const Header = () => {
-    const {user , signOutUser} = UseContext()
-   
-    const handleLogOut =()=>{
+    const { user, signOutUser ,setLoading } = UseContext()
+
+    const handleLogOut = () => {
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
@@ -17,23 +18,28 @@ const Header = () => {
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, Log Out!"
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 signOutUser()
-              Swal.fire({
-                title: "Logged Out!",
-                text: "You has been Logged Out.",
-                icon: "success"
-              });
+                setLoading(false)
+                Swal.fire({
+                    title: "Logged Out!",
+                    text: "You has been Logged Out.",
+                    icon: "success"
+                });
             }
-          });
+        });
     }
     const link = <>
         <NavLink to="/" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Home</NavLink>
         <NavLink to="/feature" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Featured Blogs</NavLink>
         <NavLink to="/blogs" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>All blogs</NavLink>
-        <NavLink to="/addBlogs" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Add Blog</NavLink>
-        <NavLink to="/wishlist" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Wishlist</NavLink>
+        {
+            user && <>
+                <NavLink to="/addBlogs" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Add Blog</NavLink>
+                <NavLink to="/wishlist" className={({ isActive }) => isActive ? "bg-emerald-500 px-2 py-1 rounded-md" : ""}>Wishlist</NavLink>
+            </>
+        }
 
     </>
     return (
@@ -41,35 +47,35 @@ const Header = () => {
 
             <Navbar fluid rounded>
                 <Navbar.Brand >
-                    
+
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"><img className='bg-gray-800 p-2 rounded-xl' src="https://i.ibb.co/5vMhs1x/logo-white.png" alt="" srcset="" /></span>
                 </Navbar.Brand>
                 <div className="flex md:order-2">
                     <Flowbite >
                         <DarkThemeToggle />
                     </Flowbite>
-                   {
-                    user ? <Dropdown
-                    arrowIcon={false}
-                    inline
-                    label={
-                        user.photoURL ?
-                        <Avatar className='ml-5' alt="User settings" img={user.photoURL} rounded />:
-                        <Avatar className='ml-5' alt="User settings" rounded />
-                      
+                    {
+                        user ? <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                user.photoURL ?
+                                    <Avatar className='ml-5' alt="User settings" img={user.photoURL} rounded /> :
+                                    <Avatar className='ml-5' alt="User settings" rounded />
+
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className="block text-sm">{user.display}</span>
+                                <span className="block truncate text-sm font-medium">{user.email}</span>
+                            </Dropdown.Header>
+                            <Dropdown.Item>Dashboard</Dropdown.Item>
+                            <Dropdown.Item>Settings</Dropdown.Item>
+                            <Dropdown.Item>Earnings</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick={handleLogOut}>Sign out</Dropdown.Item>
+                        </Dropdown> : <Link to="/signIn" className='btn'><Button className='ml-3 bg-[#db3333] '>Sign In</Button></Link>
                     }
-                >
-                    <Dropdown.Header>
-                        <span className="block text-sm">{user.display}</span>
-                        <span className="block truncate text-sm font-medium">{user.email}</span>
-                    </Dropdown.Header>
-                    <Dropdown.Item>Dashboard</Dropdown.Item>
-                    <Dropdown.Item>Settings</Dropdown.Item>
-                    <Dropdown.Item>Earnings</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Item onClick={handleLogOut}>Sign out</Dropdown.Item>
-                </Dropdown> : <Link to="/signIn" className='btn'><Button className='ml-3 bg-[#db3333] '>Sign In</Button></Link> 
-                   }
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse className='dark:text-white'>
@@ -77,7 +83,7 @@ const Header = () => {
                     {
                         link
                     }
-                   
+
                 </Navbar.Collapse>
             </Navbar>
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,16 +4,28 @@ import RecentBlogs from '../Components/RecentBlogs';
 import NewsLetter from '../Components/NewsLetter';
 import Review from '../Components/Review';
 import RatingSection from '../Components/RatingSection';
+import { motion, useScroll, useSpring } from "framer-motion";
+import Header from '../Components/Header';
+
+import "./style.css"
 
 const Home = () => {
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    });
     return (
         <div>
+            <div className="progress-bar"> <Header ></Header></div>
+            <motion.div className="progress-bar" style={{ scaleX }} />
             <Banner></Banner>
             <RecentBlogs></RecentBlogs>
             <Review></Review>
             <NewsLetter></NewsLetter>
             <RatingSection></RatingSection>
-           
+
         </div>
     );
 };
